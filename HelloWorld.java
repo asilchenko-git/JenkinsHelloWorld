f6f043b7097adacdfaf0f67ae88a0dfea6256674
asilchenko-git <77885120+asilchenko-git@users.noreply.github.com>
2021-01-23 14:18:46 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello World WELCOME to Jenkins Training -----------");
+		System.out.println("Hello World WELCOME to Jenkins Training - Made Some Changes -----------");
 	}
 }
 //843jrew329ujwdi
