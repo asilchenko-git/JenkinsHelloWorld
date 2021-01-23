bab1814a1ba942c9d12e6fc4e603cc66bad4e6e2
Silchenko-Wei <614356@bah.com>
2021-01-23 11:40:22 -0500

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World WELCOME to Jenkins Training -----------");
+	}
+}
+//843jrew329ujwdi
+//JENKINS_URL/job/HelloWorld/build?token=TOKEN_NAME
+//http://localhost:8080/jenkins/job/HelloWorld/build?token=843jrew329ujwdi
+//http://localhost:8181/job/HelloWorld/build?token=843jrew329ujwdi
